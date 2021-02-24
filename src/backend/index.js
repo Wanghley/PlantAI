8ef27b239ca70af4bfca17706af6dea90c0323fe
wanghley <wanghleys@gmail.com>
2021-02-24 15:57:07 -0300

    [REFACTORING]

---
+++ src/backend/index.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+const bodyParser = require('body-parser');
+
+const example = tf.fromPixels(webcamElement);
+const prediction = model.predict(example);
+
+
+
+
+
+app.listen(port, () => {  console.log('We are live on ' + port);});
