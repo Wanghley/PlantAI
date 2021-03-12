67a9627e098b083eff8acf99e50d1b7bde900524
Davi <davi1220022@gmail.com>
2021-03-12 17:31:07 -0300

    adicinando tela de login

--- src/app/App.js
+++ src/app/App.js
@@ -4,13 +4,16 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Resultado from './telas/Resultado';
 import Cam from './telas/Camera';
+import Login from './telas/Login';
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+      <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cam" component={Cam} />
+        
         <Stack.Screen name="Resultado" component={Resultado} />
       </Stack.Navigator>
     </NavigationContainer>
