22b4eaa830c052f53548b5fe22a1003779fda0cc
ViniAS <vini.antunes.sousa@gmail.com>
2021-03-12 12:33:04 -0300

    Agora o appp abrirá na tela Camera.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,7 @@
 import React from 'react';
-import {StyleSheet, Text, View, Button, Alert} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-import Home from './telas/Home';
-import Login from './telas/Login';
 import Resultado from './telas/Resultado';
 import Cam from './telas/Camera';
 const Stack = createStackNavigator();
@@ -13,10 +10,8 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Resultado" component={Resultado} />
         <Stack.Screen name="Cam" component={Cam} />
+        <Stack.Screen name="Resultado" component={Resultado} />
       </Stack.Navigator>
     </NavigationContainer>
   );
